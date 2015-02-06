c99704117b8fd587ecff43702f2c1566198dbe59
RemiVR <remi.vr5@gmail.com>
2015-02-06 11:06:05 +0100

    display all concerts in index, and link to a single concert info

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,6 +2,9 @@ class ConcertsController < ApplicationController
 	def index
 		@concert = Concert.all
 	end
+	def show
+		@concert = Concert.find(params[:id])
+	end
 	def new
 		@concert = Concert.new
 	end
